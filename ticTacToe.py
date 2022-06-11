680000d9c0c14b14dd5c8e09b843d9edc834ebe2
cplmills <admin@themillsclan.com>
2022-06-11 23:31:26 +1000

    uploaded v2

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,38 +1,92 @@
+from math import ceil
 board = [[1,2,3],[4,5,6],[7,8,9]]
+
+def checkSpace(space):
+    if int(space) in board[ceil(space/3)-1]:
+        return True
+    else:
+        print(f"cant find {space} in board[{ceil(space/3)-1}]")
+        return False
 
 def checkScore(board):
     #  check horizontal rows
     if board[0].count('X') == 3 or board[1].count('X') == 3 or board[2].count('X') == 3:
-        return "X"
+        return "win"
     elif board[0].count('O') == 3 or board[1].count('O') == 3 or board[2].count('O'):
-        return "O"
+        return "win"
     
     #  check vertical rows
     for i in range(3):
         if board[i][0] == 'X' and board[i][1] == 'X' and board[i][2] == 'X':
-            return "X"
+            return "win"
         elif board[i][0] == 'O' and board[i][0] == 'O' and board[i][0] == "O":
-            return "O"
+            return "win"
 
     #  check diagonal rows
     if board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O':
-        return "O"
+        return "win"
     elif board[0][2] == 'O' and board[1][1] == 'O' and board[2][0] == 'O':
-        return "O"
+        return "win"
 
     if board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X':
-        return "X"
+        return "wins"
     elif board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X':
-        return "X"
+        return "win"
+
+    allSpacesGone = True
+    for s in board:
+        for t in s:
+            print(f"{t} - {type(t)}")
+            if type(t) == 'int':
+                allSpacesGone = False
+        # if str(s) not in board:
+        #     allSpacesGone = True
+        # else:
+        #     allSpacesGone = False
+    if allSpacesGone:
+        print("all spaces gone")
+        return "drawer"
 
     return "no winner"
 
 def showBoard(board):
-    print(f"{board[0]}/n{board[1]}/n{board[2]}")
+    print()
+    print(f"{board[0][0]}|{board[0][1]}|{board[0][2]}")
+    print('-+-+-')
+    print(f"{board[1][0]}|{board[1][1]}|{board[1][2]}")
+    print('-+-+-')
+    print(f"{board[2][0]}|{board[2][1]}|{board[2][2]}")
+    print()
 
 def main():
-    showBoard(board)
-    print('score is ' + checkScore([['X','O','X'],[4,5,6],[7,8,9]]))
+    # print('score is ' + checkScore([['X','O','X'],[4,5,6],[7,8,9]]))
+    currentPlayer = "X"
+
+    while(checkScore(board)!="win") or (checkScore(board)!="drawer"):
+        showBoard(board)
+        try:
+            space = int(input("Please Choose a Space:"))
+        except:
+            print("Thanks for Playing")
+            break
+
+        if space > 9:
+            print("That is not a space")
+            continue
+        if not checkSpace(space):
+            print("That space is not available")
+            continue
+        rowNum = ceil(space/3) 
+        if rowNum-1 == 0:
+            modifier = 1
+        elif rowNum-1 == 1:
+            modifier = 4
+        else:
+            modifier = 7
+        board[rowNum-1][space-1 if space < 4 else space-modifier] = currentPlayer
+        print(currentPlayer + checkScore(board))
+        currentPlayer = "X" if currentPlayer == "O" else "O"
+
 
 if __name__ == '__main__':
     main()
---
+++ ticTacToe2.py
@@ -0,0 +1,71 @@
+from numpy import square
+import os
+
+def clear_console():
+    # clears the screen of previous text
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+def showBoard(board):
+    clear_console()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print("-+-+-")
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print("-+-+-")
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+
+
+def checkScore(board):
+    #check horizontals
+    if (board[0] == board[1] == board[2]) or (board[3] == board[4] == board[5]) or (board[6] == board[7] == board[8]):
+        return True
+
+    #check verticals
+    if (board[0] == board[3] == board[6]) or (board[1] == board[4] == board[7]) or (board[2] == board[5] == board[8]):
+        return True
+
+    # check Diagonals
+    if (board[0] == board[4] == board[8]) or (board[2] == board[4] == board[6]):
+        return True
+
+    # are there any spaces left available
+    if board.count("X") + board.count("O") == 9:
+        return 'draw'
+
+
+    return False
+
+def main():
+    board = [1,2,3,4,5,6,7,8,9]
+    currentPlayer = "X"
+    while not checkScore(board):
+        showBoard(board)
+        if checkScore(board) == 'draw':
+            break
+
+        try:
+            square = int(input("Please Choose a Space:"))-1
+        except:
+            print("Thanks for Playing")
+            break
+
+        if board[square] == "X" or board[square] == "O":
+            print("space taken, choose another one")
+            continue
+
+        if square > 9:
+            print("That is not a space")
+            continue
+
+        board[square] = currentPlayer
+        currentPlayer = "X" if currentPlayer == "O" else "O"
+
+    if checkScore(board) == "draw":
+        print("Sorry! Nobody wins!")
+    else:
+        print(f"Congratulations Player {'X' if currentPlayer == 'O' else 'O'}!")
+
+if __name__ == '__main__':
+    main()
